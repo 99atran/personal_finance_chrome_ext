7d95cda2c8adf5b33400fe05c608c5558073c02e
Brian Tran <briantran3504@gmail.com>
2020-08-04 23:04:19 -0400

    new tab on button click

--- popup.js
+++ popup.js
@@ -4,16 +4,7 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+let newTab = document.getElementById('newTab');
+newTab.onclick = function(element) {
+  chrome.tabs.create({url: "http://www.stackoverflow.com"});
 };
