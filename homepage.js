11f71605b3b17b94fe5014d3904fa721f0099075
99atran <99atran@gmail.com>
2020-08-07 22:07:47 -0400

    some funny edits, still need to work on homepage.js

--- homepage.js
+++ homepage.js
@@ -1,39 +1,46 @@
-function saveChanges() {
-  // Get a value saved in a form.
-  var textarea = document.getElementById('itemName');
-  var theValue = textarea.value;
-  console.log('now' + theValue);
-  // Check that there's some code there.
-  if (!theValue) {
-    message('Error: No value specified');
-    return;
-  }
-  // Save it using the Chrome extension storage API.
-  chrome.storage.sync.set({'key': theValue}, function() {
-    // Notify that we saved.
-    message('Settings saved');
-  });
+// function saveChanges() {
+//   // Get a value saved in a form.
+//   var textarea = document.getElementById('itemName');
+//   var theValue = textarea.value;
+//   console.log('now' + theValue);
+//   // Check that there's some code there.
+//   if (!theValue) {
+//     message('Error: No value specified');
+//     return;
+//   }
+//   // Save it using the Chrome extension storage API.
+//   chrome.storage.sync.set({'budgets_list': theValue}, function() {
+//     // Notify that we saved.
+//     message('Settings saved');
+//   });
 
-  window.alert('the value now' + theValue);
+//   window.alert('the value now' + theValue);
+// }
+
+// function getReport() {
+//   var textarea = document.getElementById('itemName');
+//   var theValue = textarea.value;
+//   chrome.storage.sync.get(['key'], function(result) {
+//     console.log('Value currently is ' + result.key);
+//     window.alert('the value is' + theValue);
+//   });
+// }
+
+function addBudget() {
+  return;
 }
 
-function getReport() {
-  var textarea = document.getElementById('itemName');
-  var theValue = textarea.value;
-  chrome.storage.sync.get(['key'], function(result) {
-    console.log('Value currently is ' + result.key);
-    window.alert('the value is' + theValue);
-  });
-}
+/* listeners here */
 
 var addItem = document.getElementById('addItem');
 var newTab = document.getElementById('newTab');
-
-addItem.onclick = function(element) {
-  preventDefault();
-  saveChanges();
-};
-
-newTab.onclick = function(element) {
-  getReport();
+if (addItem) {
+  addItem.addEventListener("click", addBudget);
 }
+
+if (newTab) {
+  newTab.onclick = function() {
+    getReport();
+  }
+}
+
