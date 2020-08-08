49b51c882c5881a6f80e1e475582d313c483594b
Brian Tran <briantran3504@gmail.com>
2020-08-08 00:17:34 -0400

    Added dynamic popup panels

--- popup.js
+++ popup.js
@@ -4,7 +4,36 @@
 
 'use strict';
 
+function printBudgetPanels(key) {
+  chrome.storage.sync.get([key], function (value) {
+
+    var panelTitle = document.createElement("p");
+    var titleTextNode = document.createTextNode(key);
+    panelTitle.appendChild(titleTextNode);
+    panelTitle.setAttribute("class", "panelTitle");
+
+    var panelStatus = document.createElement("p");
+    var statusTextNode = document.createTextNode("Broke Status: " + value[key][1].toFixed(2));
+    panelStatus.appendChild(statusTextNode);
+
+    var panel = document.createElement("div");
+    panel.appendChild(panelTitle);
+    panel.appendChild(panelStatus);
+    panel.setAttribute("class", "budgetPanel");
+    panel.setAttribute("id", key);
+
+    var ul = document.getElementById("popupPanelList");
+    ul.appendChild(panel);
+  });
+}
+
+chrome.storage.sync.get(null, function(items) {
+  var allKeys = Object.keys(items);
+  allKeys.forEach(printBudgetPanels);
+});
+
 let newTab = document.getElementById('newTab');
 newTab.onclick = function(element) {
   chrome.tabs.create({url: "homepage.html"});
 };
+
