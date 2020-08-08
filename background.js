a5b909d9abf106b4a16d7b5ea999b1aa9e126396
99atran <99atran@gmail.com>
2020-08-07 22:25:49 -0400

    should initialize budgets when installed

---
+++ background.js
@@ -0,0 +1,8 @@
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({'budget_01': 69.69});
+  chrome.storage.sync.set({'budget_02': 13.00});
+  chrome.storage.sync.set({'budget_03': 123412.00});
+  chrome.storage.sync.set({'budget_04': 3255.01});
+  chrome.storage.sync.set({'budget_05': 99.00});
+});
