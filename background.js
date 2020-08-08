65785090b92f4b78c849d76f58132daf5cd901cc
99atran <99atran@gmail.com>
2020-08-07 22:54:35 -0400

    successfully prints the contents of the data in console upon installation :)

--- background.js
+++ background.js
@@ -1,13 +1,21 @@
+function printBudgetData(key) {
+  chrome.storage.sync.get([key], function (value) {
+    console.log(key + ', with a limit of $' + value[key][1].toFixed(2) +
+     '. Current balance is $' + value[key][0].toFixed(2));
+    // console.log(value);
+  });
+}
+
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({'budget_01': 69.69});
-  chrome.storage.sync.set({'budget_02': 13.00});
-  chrome.storage.sync.set({'budget_03': 123412.00});
-  chrome.storage.sync.set({'budget_04': 3255.01});
-  chrome.storage.sync.set({'budget_05': 99.00});
+  chrome.storage.sync.set({'budget_01': [0.0, 69.69]});
+  chrome.storage.sync.set({'budget_02': [0.0, 13.00]});
+  chrome.storage.sync.set({'budget_03': [0.0, 123412.00]});
+  chrome.storage.sync.set({'budget_04': [0.0, 3255.01]});
+  chrome.storage.sync.set({'budget_05': [0.0, 99.00]});
 
   chrome.storage.sync.get(null, function(items) {
     var allKeys = Object.keys(items);
-    console.log(allKeys);
+    allKeys.forEach(printBudgetData);
   });
 });
